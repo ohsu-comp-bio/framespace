d152f63a1cbdcaeadefef002fa96b22abee9ec48
jacmarjorie <jacmarjorie@gmail.com>
2016-04-18 08:39:07 -0700

    initial commit

---
+++ configreader.py
@@ -0,0 +1,56 @@
+import json
+
+class ConfigReader:
+
+  def __init__(self, config_file):
+    """
+    Config Reader for TSV Import.
+    """
+
+    with open(config_file, "r") as conf:
+      config = json.load(conf)
+
+    # default framespace
+    self.db_name = config.get('db_name', 'framespace')
+
+    # major keyspace information
+    _ksmajor_map = getRequired(config, 'keyspace_major')
+    self.ksmajor_file = getRequired(_ksmajor_map, 'file')
+    self.ksmajor_name = getRequired(_ksmajor_map, 'name')
+    self.ksmajor_keys = getRequired(_ksmajor_map, 'keys')
+    self.ksmajor_axis = getRequired(_ksmajor_map, 'axis')
+    self.ksmajor_tsvmap = getRequired(_ksmajor_map, 'tsv_keyspace_map')
+
+    # minor keyspace information
+    _ksminor_map = getRequired(config, 'keyspace_minor')
+    self.ksminor_id = getRequired(_ksminor_map, 'id')
+    self.ksminor_name = getRequired(_ksminor_map, 'name')
+    self.ksminor_filter = _ksminor_map.get('filter', None)
+    self.ksminor_axis = getRequired(_ksminor_map, 'axis')
+ 
+    # axes
+    self.axes = config.get('axes', None)
+    # valid format correct
+    if self.axes is not None:
+      for ax in self.axes:
+        getRequired(ax, 'name')
+
+    # units
+    self.units = config.get('units', [])
+    if len(self.units) >= 1:
+      for unit in self.units:
+        getRequired(unit, 'name')
+        # units require a description
+        getRequired(unit, 'description')
+    else:
+      raise ValueError("At least one unit must be specified.")
+
+    # units
+    self.units = config.get('units', None)
+
+def getRequired(dictionary, field):
+  item = dictionary.get(field, None)
+  if item == None:
+    raise ValueError(field + " must be set in import config.")
+  return item
+
---
+++ connector.py
@@ -0,0 +1,119 @@
+import pymongo
+from pymongo import MongoClient
+import pandas as pd
+
+class Connector:
+
+  def __init__(self, database):
+    """
+    Connect to specified database, and ensure proper setup.
+    """
+    self.conn = MongoClient()
+    self.db = self.conn[database]
+
+    # check collections
+    self.axis = self.db.axis
+    self.keyspace = self.db.keyspace
+    self.units = self.db.units
+    self.vector = self.db.vector
+    self.dataframe = self.db.dataframe
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, exc_type, exc_value, traceback):
+    # create indexes, where should this happen?
+    self.keyspace = keyspace.create_index([{"keys", pymongo.ASCENDING}])
+    self.conn.close()
+
+  def registerAxes(self, axes, ksmajor_ax, ksminor_ax):
+    """
+    Register any new axes secified in config. 
+    """
+    # first register available axes in config
+    for axis in axes:
+      self.axis.update({'name': axis['name']}, axis, upsert=True)
+
+    # validate keyspace axis existence
+    self.checkAxes(ksmajor_ax)
+    self.checkAxes(ksminor_ax)
+
+  def checkAxes(self, axis_name):
+    """
+    Check to make sure keyspace axes are actually in database.
+    """
+    ax = self.axis.find_one({'name': axis_name})
+    if ax is None:
+      raise ValueError("KeySpace Axis must be registered of specified in config axes section.")
+
+
+  def registerUnits(self, units):
+    """
+    Register any newly specified units.
+    Units is a required field in the config, so no checking is necessary.
+    """
+    for unit in units:
+      self.units.update({'name': unit['name']}, unit, upsert=True)
+
+
+  def registerMajorKeySpaces(self, metadata, name, keys, axis):
+    """
+    Registration of keyspaces from a metadata file.
+    The Keyspaces to be registered are assumed to be unique. 
+    """
+    with open(metadata) as meta:
+      # construct a list of unique keyspaces
+      m_df = pd.read_table(meta)
+      kspaces = m_df[name].unique()
+
+      # construct a dictionary of keyspaces for bulk insert
+      keyspaces = []
+      for ks in kspaces:
+        ks_df = m_df[m_df[name].str.contains(ks)]
+        key_list = list(m_df[keys])
+        keyspaces.append({'name': ks, 'axis_name': axis, 'keys': key_list})
+
+      del m_df
+      self.keyspace.insert_many(keyspaces)
+
+    # return keyspaces to later use during tsv processing
+    return keyspaces
+
+
+  def registerMinorKeySpace(self, df, ksminor_id, ksminor_name, ksminor_axis):
+    """
+    Registers a minor KeySpace, any filtering is assumed to have happened prior to registration.
+    ie. ksminor_filter must occur on df before running minor ks registration.
+    """
+    # get keys
+    keys = list(df[str(ksminor_id)])
+
+    # register minor keyspace
+    minor_keyspace = {"name": ksminor_name, "axis_name": ksminor_axis, "keys": keys}
+    min_ks = self.keyspace.insert(minor_keyspace)
+
+    return min_ks
+
+
+  def registerDataFrame(self, df, ksminor_objid, units):
+    """
+    Gets respecitve major keyspace.
+    Registers the lines of the tsv file as vectors.
+    Register the tsv as a dataframe with pointer to vectors.
+    """
+
+    # get major keyspace
+    keys = df.columns.tolist()[1:]
+    md_ks = self.keyspace.find({"keys": keys[0]})
+    if set(keys) <= set(md_ks[0]['keys']):
+      maj_dim = md_ks[0]['_id']
+    else:
+      raise ValueError("Not all keys in dimension are registered to keyspace.")
+
+    # vectors are inserted into dataframe as ids to get around data storage limit
+    vectors = self.vector.insert_many(df.reset_index(drop=True).to_dict(orient='records'))
+    dataframe = {"major": md_ks[0]['_id'], "minor": ksminor_objid, "units": units, "contents": list(vectors.inserted_ids)}
+
+    _id = self.dataframe.insert_one(dataframe)
+    return _id
+
