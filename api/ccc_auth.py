b8f302095884a41fba5103f5668da01f6ebefe89
jacmarjorie <jacmarjorie@gmail.com>
2016-09-14 12:29:40 -0700

    updates to ccc auth

--- api/ccc_auth.py
+++ api/ccc_auth.py
@@ -12,13 +12,20 @@ def validateRulesEngine(request):
   print dir(request)
   print request.url
   method = request.method
-  resource = request.url
-  jwt_token = str(request.headers['Authorization'].split(' ')[1])
-
-  r = requests.post('http://localhost:5000/v1/ruleEngine/validation', \
-    data = json.dumps({'token': jwt_token, 'action':str(method), 'resources': str(resource)}), \
+  resource = request.path
+  if request.query_string:
+    resource += "?"+request.query_string
+  print "resource:", resource
+  try:
+    jwt_token = str(request.headers['Authorization'].split(' ')[1])
+  except:
+    return False	
+  print jwt_token
+  r = requests.post('http://192.168.11.252:8903/v1/ruleEngine/validation', \
+    data = json.dumps({'token': jwt_token, 'action':str(method), 'resources': [str(resource)]}), \
     headers={'content-type': 'application/json'})
-
+  print r
+  print r.content
   if strToBool(r.content):
     return True
 
